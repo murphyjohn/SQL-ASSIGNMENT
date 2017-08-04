f2ba333609c2cfbe06622ed523ff63bffd6e4c4a
John Murphy <murphyjohn@users.noreply.github.com>
2017-08-04 08:34:22 +0100

    Update request11.sql

--- request11.sql
+++ request11.sql
@@ -2,10 +2,11 @@
 --List the department number and name for all departments where no
 --programmers work.
 
-select distinct d.Department_No, Department_Name
-from Departments d, Employees e, Jobs j
-where d.Department_No=e.Department_No
-and 
-	e.Job_ID = j.Job_ID
-and	
-	Job_Title='Programmer'
+Use CGDatabase
+
+select distinct d.Department_Name, d.Department_No
+from Departments d, Employees e
+Group by d.Department_No, d.Department_Name
+except (select d.Department_Name,e.Department_No from Departments d, Employees e, Jobs j
+where j.Job_ID = e.Job_ID
+and Job_Title = 'Programmer')
