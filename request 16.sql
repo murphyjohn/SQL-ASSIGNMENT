47e8f7d2f843513e695928410557834b2e84b1f5
John Murphy <murphyjohn@users.noreply.github.com>
2017-08-08 11:24:46 +0100

    Update request 16.sql
    Changed from using multiple unions to adding case statements in the select query.

--- request 16.sql
+++ request 16.sql
@@ -3,50 +3,21 @@
 --and old and new salaries. Employees in departments 20 and 10 are given a 5% rise,
 --employees in departments 50, 80, 90 and 110 are given a 10% rise
 --and employees in other departments are not given a rise.
-use CGDatabase
 
-select distinct
-		First_Name, Last_Name, Employee_No, Annual_Salary as Old_Salary,
-		Round((Annual_Salary*1.05),2) as New_Salary, '5%' as Raise
-		 
-from Employees e, Departments d
-where e.Department_no = d.Department_no
-and
-    e.Department_No = 10
-	or
-	e.Department_No=20
+select Employee_No, 
+Annual_Salary Old_Salary,
+	case
+	when Department_No in (20, 10) then Annual_Salary*1.05
+	when Department_No in (50, 80, 90, 110) then Annual_Salary*1.10
+	else Annual_Salary
+	end as New_Salary,
+	
+	case
+	when Department_No in (20, 10) then '5%'
+	when Department_No in (50, 80, 90, 110) then '10%'
+	else '0%'
+	end as Percentage_Raise,
 
-union 
- select distinct
-		First_Name, Last_Name, Employee_No, Annual_Salary as Old_Salary,
-		Round((Annual_Salary*1.10),2) as New_Salary, '10%' as Raise
-		 
-from Employees e, Departments d
-where e.Department_no = d.Department_no
-and
-    e.Department_No = 50
-	or
-	e.Department_No=80
-	or
-	e.Department_No=90
-	or 
-	e.Department_No=110
-union 
- select distinct
-		First_Name, Last_Name, Employee_No, Annual_Salary as Old_Salary,
-		Annual_Salary as New_Salary, '0%' as Raise
-		from Employees e, Departments d
-where e.Department_no = d.Department_no
-and
-    e.Department_No!= 50
-	and
-	e.Department_No!=80
-	and
-	e.Department_No!=90
-	and 
-	e.Department_No!=110
-	and
-	e.Department_No!=20
-	and
-	e.Department_No!=10
-
+Department_No
+from Employees
+order by Department_No
