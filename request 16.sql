dea18c9356feeb77c6d3c4fb0428f1c3448093c9
John Murphy <murphyjohn@users.noreply.github.com>
2017-08-08 12:08:11 +0100

    Update request 16.sql

--- request 16.sql
+++ request 16.sql
@@ -4,20 +4,19 @@
 --employees in departments 50, 80, 90 and 110 are given a 10% rise
 --and employees in other departments are not given a rise.
 
-select Employee_No, 
-Annual_Salary Old_Salary,
+select 
+	Employee_No, 
+	Department_No,
+	Annual_Salary Old_Salary,
 	case
 		when Department_No in (20, 10) then Annual_Salary*1.05
 		when Department_No in (50, 80, 90, 110) then Annual_Salary*1.10
 		else Annual_Salary
 	end as New_Salary,
-	
 	case
 		when Department_No in (20, 10) then '5%'
 		when Department_No in (50, 80, 90, 110) then '10%'
 		else '0%'
-	end as Percentage_Raise,
-
-Department_No
+	end as Percentage_Raise
 from Employees
 order by Department_No
