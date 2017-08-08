2d8865ddd5bfc4b3574d6d386f9d749eb8bc4e37
John Murphy <murphyjohn@users.noreply.github.com>
2017-08-08 11:37:37 +0100

    Update request 11.sql
    Added indentation.

--- request 11.sql
+++ request 11.sql
@@ -7,6 +7,7 @@ Use CGDatabase
 select distinct d.Department_Name, d.Department_No
 from Departments d, Employees e
 Group by d.Department_No, d.Department_Name
-except (select d.Department_Name,e.Department_No from Departments d, Employees e, Jobs j
-where j.Job_ID = e.Job_ID
-and Job_Title = 'Programmer')
+except 
+	(select d.Department_Name,e.Department_No from Departments d, Employees e, Jobs j
+		where j.Job_ID = e.Job_ID
+		and Job_Title = 'Programmer')
